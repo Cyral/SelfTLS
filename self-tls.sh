5222fe713793afbf8236f2de1a2d9d2717ecea3e
Cyral <cyral@pyratron.com>
2020-05-12 17:29:28 -0400

    Fix filename error

--- self-tls.sh
+++ self-tls.sh
@@ -13,6 +13,6 @@ openssl req -new -x509 -days 3650 -key "$domain-ca.key" -subj "/C=US/ST=$state/O
 echo "Generating certificate..."
 openssl req -newkey rsa:2048 -nodes -keyout "$domain.localhost.key" -subj "/C=US/ST=$state/O=$org./CN=*.$domain.localhost" -out "$domain.localhost.csr"
 openssl x509 -req -extfile <(printf "subjectAltName=DNS:*.$domain.localhost,DNS:$domain.localhost") -days 3650 -in "$domain.localhost.csr" -CA "$domain-ca.crt" -CAkey  "$domain-ca.key" -CAcreateserial -out  "$domain.localhost.crt"
-cat "$domain-ca.crt" "$domain.localhost.key" > "$domain.localhost.pem"
+cat "$domain.localhost.crt" "$domain.localhost.key" > "$domain.localhost.pem"
 echo "Done!"
 echo "For nginx, use the .pem with ssl_certificate and the .key with ssl_certificate_key"
