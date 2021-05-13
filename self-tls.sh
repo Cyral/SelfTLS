af2aa8acd4152a18c557ad70a4a83fbe89af91b3
Heath <Cyral@users.noreply.github.com>
2021-05-13 16:45:35 -0400

    Use -sha256 option

--- self-tls.sh
+++ self-tls.sh
@@ -8,11 +8,11 @@ echo "Input host (ex: example, which will make a *.example.localhost key)"
 read domain
 echo "Generating Root Certifcate Authority..."
 openssl genrsa -out "$domain-ca.key" 2048
-openssl req -new -x509 -days 3650 -key "$domain-ca.key" -subj "/C=US/ST=$state/O=$org/CN=$org CA" -out "$domain-ca.crt"
+openssl req -new -x509 -sha256 -days 3650 -key "$domain-ca.key" -subj "/C=US/ST=$state/O=$org/CN=$org CA" -out "$domain-ca.crt"
 
 echo "Generating certificate..."
-openssl req -newkey rsa:2048 -nodes -keyout "$domain.localhost.key" -subj "/C=US/ST=$state/O=$org./CN=*.$domain.localhost" -out "$domain.localhost.csr"
-openssl x509 -req -extfile <(printf "subjectAltName=DNS:*.$domain.localhost,DNS:$domain.localhost") -days 3650 -in "$domain.localhost.csr" -CA "$domain-ca.crt" -CAkey  "$domain-ca.key" -CAcreateserial -out  "$domain.localhost.crt"
+openssl req -newkey rsa:2048 -sha256 -nodes -keyout "$domain.localhost.key" -subj "/C=US/ST=$state/O=$org./CN=*.$domain.localhost" -out "$domain.localhost.csr"
+openssl x509 -sha256 -req -extfile <(printf "subjectAltName=DNS:*.$domain.localhost,DNS:$domain.localhost") -days 3650 -in "$domain.localhost.csr" -CA "$domain-ca.crt" -CAkey  "$domain-ca.key" -CAcreateserial -out  "$domain.localhost.crt"
 cat "$domain.localhost.crt" "$domain.localhost.key" > "$domain.localhost.pem"
 echo "Done!"
 echo "For nginx, use the .pem with ssl_certificate and the .key with ssl_certificate_key"
